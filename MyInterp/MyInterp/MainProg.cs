67c9c168ce764130c0974ec1d01337a985c11500
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2021-12-27 21:13:47 +0700

    mylang

---
+++ MyInterp/MyInterp/MainProg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyInterp
+{
+    class MainProg
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\I am Photoshop, it is a Programmer\Visual Studio\C#\mylang\mylang\test.txt";
+            if (File.Exists(filePath))
+            {
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filePath).ToList();
+
+                var res =LexicAnalyser.Analyze(lines);
+                var ops = OPSGenerator.GenerateOPS(res);
+            }
+        }
+    }
+}
